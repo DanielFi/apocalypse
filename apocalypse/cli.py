8da826ee9497cde2f74a4cd5b481bb989e5af173
Daniel Firer <firerdaniel@gmail.com>
2021-02-11 18:48:08 +0200

    Implement timeline CLI
    You can now initialize 'timelines', which contain inserted versions and track
    class names across them.
    
    The old 'diff' command was refactored into a flag of the new 'map' command.

--- apocalypse/cli.py
+++ apocalypse/cli.py
@@ -1,9 +1,9 @@
 import click
 import logging
-
 import json
-
+from pathlib import Path
 from apocalypse.differ import DexDiffer
+import apocalypse.timeline as timeline
 
 
 @click.group()
@@ -13,18 +13,67 @@ def main(verbose):
         logging.basicConfig(level=logging.INFO)
 
 @main.command()
-@click.option('-r', '--reverse', is_flag=True)
-@click.argument('old', type=click.Path(exists=True))
-@click.argument('new', type=click.Path(exists=True))
-def diff(old: str, new: str, reverse: bool):
-    """Diff two DEX files
-    
-    OLD is the old DEX file
-    NEW is the new DEX file
+@click.argument('name')
+def init(name: str):
+    """Initialize a new timeline. 
     """
-    differ = DexDiffer()
-    mapping, reverse_mapping = differ.diff(old, new)
-    click.echo(json.dumps(reverse_mapping if reverse else mapping))
+    timeline.init(name)
+
+@main.command()
+@click.argument('version')
+@click.argument('dex', type=click.Path(exists=True))
+@click.option('-f', '--force', is_flag=True)
+@click.option('--compute/--no-compute', default=True)
+def insert(version: str, dex: str, force: bool, compute: bool):
+    """Insert a version into the timeline. 
+    """
+    timeline.insert_version(version, dex, force, compute)
+
+@main.command()
+def versions():
+    """Show versions in timeline. 
+    """
+    result = timeline.versions()
+    if result:
+        click.echo('\n'.join(result))
+
+@main.command()
+@click.option('--version/--dex', default=True, help='Chose whether to compare versions or dex files. ')
+@click.argument('from_', metavar='FROM')
+@click.argument('to')
+def map(from_: str, to: str, version: bool):
+    """Map classes from one version to another. 
+    """
+    if version:
+        click.echo(timeline.map(from_, to))
+    else:
+        if not Path(from_).is_file():
+            click.echo(f"Error: Invalid value for 'FROM': Path '{from_}' does not exist. \n")
+            return
+        if not Path(to).is_file():
+            click.echo(f"Error: Invalid value for 'TO': Path '{to}' does not exist. \n")
+            return
+    
+        differ = DexDiffer()
+        mapping, _ = differ.diff(from_, to)
+        click.echo(json.dumps(mapping))
+
+@main.command()
+@click.argument('version')
+@click.argument('class_', metavar='CLASS')
+def until(version, class_):
+    """Print the last version where CLASS from VERSION existed. 
+    """
+    click.echo(f"Class '{class_}' existed until version {timeline.until(version, class_)}")
+
+@main.command()
+@click.argument('version')
+@click.argument('class_', metavar='CLASS')
+def since(version, class_):
+    """Print the first version where CLASS from VERSION appeared. 
+    """
+    click.echo(f"Class '{class_}' existed since version {timeline.since(version, class_)}")
+
 
 if __name__ == '__main__':
     main()
